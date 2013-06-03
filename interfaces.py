6bc47e6597f40e449b076f3d768e01c687d5471f
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-02 18:31:09 -0700

    renames ip/subscriber list. adds init function to init this list

--- interfaces.py
+++ interfaces.py
@@ -1,17 +1,19 @@
 class MessageReceiverInterface:
     """A message receiver interface"""
     # Sets up the stuff a receiver might need
-    def setup(self):
+    def __init__(self):
+        self.subscriberList = []
+    def setup(self, osc, loc):
         print "setup not implemented"
     # Checks for new messages
     def update(self):
         print "update not implemented"
     # Adds a new subscriber to a receiver
     def addSubscriber(self, ip):
-        self.ipList.append(ip)
+        self.subscriberList.append(ip)
     # Removes subscriber from receiver
     def removeSubscriber(self, ip):
         try:
-            self.ipList.remove(ip)
+            self.subscriberList.remove(ip)
         except:
             pass
