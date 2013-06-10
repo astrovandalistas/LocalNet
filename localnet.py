d69d5ce0d62edf32badaaf847b3e1bbead26374d
Thiago Hersan <thiago.hersan+github@gmail.com>
2013-06-10 00:08:58 -0700

    renames receiver/prototype dict variables

--- localnet.py
+++ localnet.py
@@ -4,27 +4,31 @@ import time
 from OSC import OSCClient
 from receivers import TwitterReceiver, SmsReceiver, OscReceiver
 
+# these will probably be command line arguments
+LOCAL_NET_LOCALE = "Five42"
+
 def setup():
-    global rcvrs, prots
-    rcvrs = {}
-    prots = {}
+    global receivers, prototypes
+    receivers = {}
+    prototypes = {}
     rcvT = TwitterReceiver()
-    rcvrs['twitter'] = rcvT
+    receivers['twitter'] = rcvT
     #rcvS = SmsReceiver()
-    #rcvrs['sms'] = rcvS
-    rcvO = OscReceiver(rcvrs,prots)
+    #receivers['sms'] = rcvS
+    rcvO = OscReceiver(receivers,prototypes)
     oc = OSCClient()
-    for (k,v) in rcvrs.iteritems():
-        v.setup(oc,"LocalLocalNet")
+    for (k,v) in receivers.iteritems():
+        v.setup(oc,LOCAL_NET_LOCALE)
 
 def loop():
-    for (k,v) in rcvrs.iteritems():
+    for (k,v) in receivers.iteritems():
         v.update()
 
 if __name__=="__main__":
     setup()
     try:
         while(True):
+            ## TODO: check prots every N seconds
             ## keep it from looping faster than ~60 times per second
             loopStart = time.time()
             loop()
@@ -33,5 +37,5 @@ if __name__=="__main__":
                 time.sleep(0.017 - loopTime)
 
     except KeyboardInterrupt :
-        for (k,v) in rcvrs.iteritems():
+        for (k,v) in receivers.iteritems():
             v.stop()
